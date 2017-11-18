cf4a6b4c6f2db53020bfb311a88785b1d119ad7a
sapandesai11 <sapandesai11@gmail.com>
2017-11-18 23:59:11 +0530

    Changes related to check Email Notification for Jenkins Build failure

--- src/test/java/com/sd/springboot/services/impl/UserServiceImplTest.java
+++ src/test/java/com/sd/springboot/services/impl/UserServiceImplTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sd.springboot.config.Application;
@@ -18,9 +17,8 @@ import com.sd.springboot.services.UserService;
 
 
 
-@SpringBootTest(classes={Application.class})
+//@SpringBootTest(classes={Application.class})
 @RunWith(SpringRunner.class)
-@TestPropertySource(locations= "classpath:application-test.properties")
 public class UserServiceImplTest {
 	
 	
